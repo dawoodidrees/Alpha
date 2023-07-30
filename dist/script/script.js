f1c1a99effd0a92b26ad97dcc937ffcda541c8cd
daudidrees <m.daudidrees@gmail.com>
2023-07-30 14:42:40 +0500

    added swiper slider

--- dist/script/script.js
+++ dist/script/script.js
@@ -8,3 +8,35 @@ function toggleDescription(event) {
 accordions.forEach((accordion) => {
   accordion.addEventListener("click", toggleDescription);
 });
+
+// Swiper
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 5,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+  },
+  breakpoints: {
+    350: {
+      slidesPerView: 3,
+      spaceBetweenSlides: 30,
+    },
+
+    // when window width is <= 499px
+    499: {
+      slidesPerView: 3,
+      spaceBetweenSlides: 30,
+    },
+
+    1000: {
+      slidesPerView: 3,
+      spaceBetweenSlides: 30,
+    },
+    // when window width is <= 999px
+    1200: {
+      slidesPerView: 5,
+      spaceBetweenSlides: 40,
+    },
+  },
+});
