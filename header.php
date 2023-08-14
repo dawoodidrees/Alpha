891e16c23d9ecbda09ffe22285d895a13b27ad78
daudidrees <m.daudidrees@gmail.com>
2023-08-14 23:06:26 +0500

    added hamburger and made new nav menu

--- header.php
+++ header.php
@@ -75,8 +75,36 @@
         }
       ?>
 
-      <!-- <div>
-        <img loading="lazy" src="images/searchbar.png" alt="Logo" />
-      </div> -->
+      <div class="navbar__hamburger">
+        <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" class="icon-hamburger" fill="none" viewBox="0 0 18 16">
+          <path d="M1 .5a.5.5 0 100 1h15.71a.5.5 0 000-1H1zM.5 8a.5.5 0 01.5-.5h15.71a.5.5 0 010 1H1A.5.5 0 01.5 8zm0 7a.5.5 0 01.5-.5h15.71a.5.5 0 010 1H1a.5.5 0 01-.5-.5z" fill="currentColor">
+          </path>
+        </svg>
+
+        <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" class="icon-close" fill="none" viewBox="0 0 18 17">
+          <path d="M.865 15.978a.5.5 0 00.707.707l7.433-7.431 7.579 7.282a.501.501 0 00.846-.37.5.5 0 00-.153-.351L9.712 8.546l7.417-7.416a.5.5 0 10-.707-.708L8.991 7.853 1.413.573a.5.5 0 10-.693.72l7.563 7.268-7.418 7.417z" fill="currentColor">
+          </path>
+        </svg>
+      </div>
+
     </header>
+
+    <script>
+      const hamburger = document.querySelector('.navbar__hamburger');
+      const navDrawer = document.querySelector('.navbar__links');
+      const iconClose = document.querySelector('.icon-close');
+      const iconHamburger = document.querySelector('.icon-hamburger');
+      const bodyEl = document.body
+
+      hamburger.addEventListener('click', () => {
+        navDrawer.classList.toggle('navbar__links-active');
+        const isActive = navDrawer.classList.contains('navbar__links-active');
+
+        if(isActive) bodyEl.classList.add('disable-body')
+        else bodyEl.classList.remove('disable-body')
+
+        iconClose.style.display = isActive ? 'block' : 'none';
+        iconHamburger.style.display = isActive ? 'none' : 'block';
+      });
+   </script>
 
