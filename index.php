63efc6dcbd6ec088c060b1ff8b3bc9cf5ede1608
daudidrees <m.daudidrees@gmail.com>
2023-08-19 02:59:04 +0500

    added sidebar to blog page

--- index.php
+++ index.php
@@ -15,22 +15,24 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<main id="primary" class="site-main">
 
-		<?php
+  <?php
 		if ( have_posts() ) :
 
 			if ( is_home() && ! is_front_page() ) :
 				?>
-				<section id="post-<?php the_ID(); ?>" <?php post_class(['banner', 'page-width']); ?>>
-          <h1 class="page-title h1"><?php single_post_title(); ?></h1>
-        <div class="banner__bg">
-          <?php alpha_post_thumbnail(); ?>
-        </div>
-      </section>
-				<?php
-			endif;
+  <section id="post-<?php the_ID(); ?>" <?php post_class(['banner', 'page-width']); ?>>
+    <h1 class="page-title h1"><?php single_post_title(); ?></h1>
+    <div class="banner__bg">
+      <?php alpha_post_thumbnail(); ?>
+    </div>
+  </section>
+  <?php endif; ?>
 
+  <div class="blog__page">
+    <div class="blog-posts__wrapper">
+      <?php
 			/* Start the Loop */
 			while ( have_posts() ) :
 				the_post();
@@ -40,11 +42,15 @@ get_header();
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
 				 */
+				?>
+
+      <?php
 				get_template_part( 'template-parts/content', 'archive' );
 
 			endwhile;
 
 			the_posts_navigation();
+
 
 		else :
 
@@ -52,8 +58,14 @@ get_header();
 
 		endif;
 		?>
+    </div>
+    <?php
+		get_sidebar();
+		?>
+  </div>
 
-	</main><!-- #main -->
+
+</main><!-- #main -->
 
 <?php
 get_footer();
--- template-parts/content-archive.php
+++ template-parts/content-archive.php
@@ -1,4 +1,4 @@
-<div class="page-width blog__archive">
+<div class="page-width">
   <div class="blog__article">
     <?php
 		if ( is_singular() ) {
@@ -6,19 +6,18 @@
     } else {
 			the_title( '<h2 class="h2 "><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
     }
-    // $time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
-    // $time_string = sprintf(
-		// 	$time_string,
-		// 	esc_attr( get_the_date( DATE_W3C ) ),
-		// 	esc_html( get_the_date() ),
-		// 	esc_attr( get_the_modified_date( DATE_W3C ) ),
-		// 	esc_html( get_the_modified_date() )
-		// );
-    // $posted_on = sprintf( esc_html_x( 'Posted on %s', 'post date', 'alpha' ),$time_string );
-    //   echo '<p class="posted-on">' . $posted_on . '</p>';
+    $time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+    $time_string = sprintf(
+			$time_string,
+			esc_attr( get_the_date( DATE_W3C ) ),
+			esc_html( get_the_date() ),
+			esc_attr( get_the_modified_date( DATE_W3C ) ),
+			esc_html( get_the_modified_date() )
+		);
+    $posted_on = sprintf( esc_html_x( 'Posted on %s', 'post date', 'alpha' ),$time_string );
+      echo '<p class="posted-on">' . $posted_on . '</p>';
 
-    the_excerpt();
-    ?>
+    the_excerpt(); ?>
   </div>
 
 </div>
